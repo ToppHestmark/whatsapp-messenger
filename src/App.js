2e96426cc58d03a98913628dd6415e7dfc902a6a
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-26 17:42:01 +0200

    added router on the App page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import './App.css';
 import Chat from './components/Chat';
 import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
-        <Sidebar />
-        <Chat />
+        <Router>
+          <Switch>
 
+            <Route path="/app">
+              <Sidebar />
+              <Chat />
+            </Route>
+
+            <Route path="/">
+              <h1>Home</h1>
+            </Route>
+
+          </Switch>
+        </Router>
       </div>
     </div>
   );
