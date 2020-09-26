dfa0a85708575e47178dfe499d900e47653d7854
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-26 22:10:05 +0200

    fixed id dependency on SidebarChat.js

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -19,7 +19,7 @@ function SidebarChat({ id, addNewChat, name }) {
         setMessages(snapshot.docs.map(doc => doc.data()))
       ));
     }
-  }, [])
+  }, [id])
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000))
