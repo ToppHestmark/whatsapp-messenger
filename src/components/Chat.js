ba700a6c3458f63afe9588d0fd127e94c6b3ff4a
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-26 22:02:56 +0200

    added last message function for the sidebar chat room overview

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -58,7 +58,9 @@ function Chat() {
 
         <div className="chat__headerInfo">
           <h3> {roomName} </h3>
-          <p>Last seen...</p>
+          <p>Last seen{' '} 
+           {new Date(messages[messages.length - 1]?.timestamp?.toDate()).toUTCString()}
+          </p>
         </div>
 
         <div className="chat__headerRight">
